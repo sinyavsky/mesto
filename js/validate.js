89dcb04756ff08aee0298498d3a8c40558fb06d3
Alexander Sinyavsky <paul_norck@hotmail.com>
2020-12-19 14:14:52 +0300

    refac: вынести функции из enableValidation в глобальную область видимости

--- js/validate.js
+++ js/validate.js
@@ -15,57 +15,54 @@ function toggleSubmitButton(buttonItem, inputsList, inactiveButtonClass) {
   }
 }
 
+function showValidityError(formItem, inputItem, errorText, settings) {
+  inputItem.classList.add(settings.inputErrorClass);
+
+  const errorItem = formItem.querySelector(`.${inputItem.id}-error`);
+  errorItem.classList.add(settings.errorClass);
+  errorItem.textContent = errorText;
+}
+
+function hideValidityError(formItem, inputItem, settings) {
+  inputItem.classList.remove(settings.inputErrorClass);
+
+  const errorItem = formItem.querySelector(`.${inputItem.id}-error`);
+  errorItem.classList.remove(settings.errorClass);
+  errorItem.textContent = '';
+}  
+
+function checkValidity(formItem, inputItem, settings) {
+  if(inputItem.validity.valid) {
+    hideValidityError(formItem, inputItem, settings);
+  }
+  else {
+    showValidityError(formItem, inputItem, inputItem.validationMessage, settings);
+  }
+}   
+
+function setEventListeners(formsList, settings) {
+  formsList.forEach((formItem) => {
+    const inputsList = Array.from(formItem.querySelectorAll(settings.inputSelector));
+    const buttonItem = formItem.querySelector(settings.submitButtonSelector);
+    toggleSubmitButton(buttonItem, inputsList, settings.inactiveButtonClass);
+    inputsList.forEach(function (inputItem) {
+      inputItem.addEventListener('input', function () {
+        checkValidity(formItem, inputItem, settings);
+        toggleSubmitButton(buttonItem, inputsList, settings.inactiveButtonClass);
+      });        
+    });
+  });
+}
+
 function enableValidation(settings) {
 
   const formsList = Array.from(document.querySelectorAll(settings.formSelector));
 
-  function preventFormsDefault() {
-    formsList.forEach((formItem) => {
-      formItem.addEventListener('submit', (evt) => {
-        evt.preventDefault();
-      });
+  formsList.forEach((formItem) => {
+    formItem.addEventListener('submit', (evt) => {
+      evt.preventDefault();
     });
-  }
+  });
 
-  function showValidityError(formItem, inputItem, errorText) {
-    inputItem.classList.add(settings.inputErrorClass);
-
-    const errorItem = formItem.querySelector(`.${inputItem.id}-error`);
-    errorItem.classList.add(settings.errorClass);
-    errorItem.textContent = errorText;
-  }
-  
-  function hideValidityError(formItem, inputItem) {
-    inputItem.classList.remove(settings.inputErrorClass);
-
-    const errorItem = formItem.querySelector(`.${inputItem.id}-error`);
-    errorItem.classList.remove(settings.errorClass);
-    errorItem.textContent = '';
-  }  
-
-  function checkValidity(formItem, inputItem) {
-    if(inputItem.validity.valid) {
-      hideValidityError(formItem, inputItem);
-    }
-    else {
-      showValidityError(formItem, inputItem, inputItem.validationMessage);
-    }
-  }
-
-  function setEventListeners() {
-    formsList.forEach((formItem) => {
-      const inputsList = Array.from(formItem.querySelectorAll(settings.inputSelector));
-      const buttonItem = formItem.querySelector(settings.submitButtonSelector);
-      toggleSubmitButton(buttonItem, inputsList, settings.inactiveButtonClass);
-      inputsList.forEach(function (inputItem) {
-        inputItem.addEventListener('input', function () {
-          checkValidity(formItem, inputItem);
-          toggleSubmitButton(buttonItem, inputsList, settings.inactiveButtonClass);
-        });        
-      });
-    });
-  }
-
-  preventFormsDefault(); 
-  setEventListeners();  
+  setEventListeners(formsList, settings);   
 }
