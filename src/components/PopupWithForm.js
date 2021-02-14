8b92e68c461d1c64dd49b887f0d66657b894ec1d
Alexander Sinyavsky <paul_norck@hotmail.com>
2021-02-14 11:46:25 +0300

    refac: убрать лишние комментарии

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -7,15 +7,10 @@ export default class PopupWithForm extends Popup {
     super(popupSel);
 
     this._submitEvent = submitEvent;
-    // думаю ничего страшного, что обращение не по классу
-    // т.к. подразумевается, что в поп-апе у нас только 1 форма
     this._form = this._popup.querySelector('form');
   }
 
   _getInputValues() {
-    // а вот здесь мне кажется уместна выборка по классу
-    // т.к. в теории у нас может быть форма и с textarea, и с input[type=submit] (содержимое которого собирать не надо)
-    // но в брифе не указано, что мы можем дополнительно передавать в класс еще и селектор инпутов, поэтому выбираю по тегу
     const inputs = this._form.querySelectorAll('input');
     const formData = {};
 
