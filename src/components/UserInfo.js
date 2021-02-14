c028657b905a177702e1dc9dc962b8f6acc85c85
Alexander Sinyavsky <paul_norck@hotmail.com>
2021-02-14 13:27:39 +0300

    feat: добавить аватар в класс

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,17 +1,19 @@
 export default class UserInfo {
 
-  constructor({nameSel, bioSel}) {
+  constructor({nameSel, bioSel, avaSel}) {
     this._name = document.querySelector(nameSel);
     this._bio = document.querySelector(bioSel);
+    this._ava = document.querySelector(avaSel);
   }
 
   getUserInfo() {
     return {name: this._name.textContent, bio: this._bio.textContent}
   }
 
-  setUserInfo({name, bio}) {
+  setUserInfo({name, bio, ava}) {
     this._name.textContent = name;
     this._bio.textContent = bio;
+    this._ava.src = ava;
   }
 
 }
